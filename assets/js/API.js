564863819e6d8a00e4a2f007212cc3b4b8768640
Fatma Elbahas <154802371+FatmaElbahas@users.noreply.github.com>
2025-05-15 04:00:42 +0300

    Update API.js

--- assets/js/API.js
+++ assets/js/API.js
@@ -1,10 +1,10 @@
 function showLoader() {
-  $(".loading").fadeIn();
+  $(".loading").fadeIn(1000);
   $("body").addClass("no-scroll"); // يمنع التمرير
 }
 
 function hideLoader() {
-  $(".loading").fadeOut();
+  $(".loading").fadeOut(1000);
   $("body").removeClass("no-scroll"); // يرجّع التمرير
 }
 async function getMeals() {
